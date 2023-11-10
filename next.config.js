5365d5e40eb0c13004118663b57a9fa073db7811
RodrigoGimenez <rodrigogfxs@gmail.com>
2023-11-10 14:40:23 -0300

    Mejoras en el index

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["fakestoreapi.com"],
+    domains: [
+      "fakestoreapi.com",
+      "images.pexels.com"
+    ],
   },
 };
 
--- src/components/Inicio.tsx
+++ src/components/Inicio.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import style from '@/styles/index.module.css'
 import Image from 'next/image'
 import BotonCategory from './BotonCategory'
-
+import Link from 'next/link'
 function Inicio() {
   return (
     <>
@@ -13,28 +13,34 @@ function Inicio() {
         </div>
         <BotonCategory />
         <div className={style.flex}>
+            <div className={style.imagecard}>
+          <Link href={`/productos/category/${encodeURIComponent("women's clothing")}`}>
+                <Image src="https://images.pexels.com/photos/1559920/pexels-photo-1559920.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt='banner ropa'
+                width={100}
+                height={200}
+                layout="responsive"
+                className={style.banner}
+                />
+          </Link>
+                <h2>Todo lo nuevo lo tenemos aca!</h2>
+            </div>
           <div className={style.imagecard}>
-              <Image src="/images/stylewoman.webp" alt='banner ropa'
+          <Link href={`/productos/category/${encodeURIComponent("men's clothing")}`}>
+              <Image src="https://images.pexels.com/photos/775279/pexels-photo-775279.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt='banner ropa'
               width={100}
-              height={100}
-              layout='resposive'
+              height={200}
+              layout="responsive"
               className={style.banner}
               />
-              <h2>Todo lo nuevo lo tenemos aca!</h2>
-          </div>
-          <div className={style.imagecard}>
-              <Image src="/images/stylemen.webp" alt='banner ropa'
-              width={100}
-              height={100}
-              layout='resposive'
-              className={style.banner}
-              />
+              </Link>
               <h2>Todo lo nuevo lo tenemos aca!</h2>
           </div>
         </div>
         <h2 className={style.title}>Productos Gaming</h2>
         <p className={style.subtitle}>Contamos con una gran variedad de productos Gaming</p>
-        <Image src="/images/gaming-image.webp" alt='Imagen Gaming' width={100} height={100} className={style.imagegame}/>
+        <Link href={"/productos/category/electronics"}>
+          <Image src="https://images.pexels.com/photos/7915357/pexels-photo-7915357.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt='Imagen Gaming' width={100} height={100} layout="responsive" className={style.imagegame}/>
+        </Link>
     </section>
     </>
   )
