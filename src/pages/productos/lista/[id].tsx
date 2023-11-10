211f915f065dee51e59f56f1fff26906068203a1
NehemiasBalsas <brandonbalsas7@gmail.com>
2023-11-10 12:21:33 -0300

    hecho el cart,falta terminar

--- src/pages/productos/lista/[id].tsx
+++ src/pages/productos/lista/[id].tsx
@@ -1,9 +1,12 @@
-import Nav from "@/components/nav/Nav";
-import Image from "next/image";
-import style from "@/styles/detalleproducto.module.css";
-import React from "react";
-import Link from "next/link";
-interface Product {
+import React, { useContext, useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import Image from 'next/image';
+import Nav from '@/components/nav/Nav';
+import style from '@/styles/detalleproducto.module.css';
+import { CartContext } from '@/context/CartContext';
+
+
+interface Producto {
   id: number;
   title: string;
   description: string;
@@ -12,47 +15,53 @@ interface Product {
   image: string;
 }
 
-const DetalleDelProducto = ({ producto }: { producto: Product }) => {
+const DetalleDelProducto: React.FC = () => {
+  const router = useRouter();
+  const { id } = router.query;
+  const [producto, setProducto] = useState<Producto | null>(null);
+  
+  //@ts-ignore
+  const { cart, setCart } = useContext(CartContext);
+  
+  useEffect(() => {
+    const obtenerDetalleProducto = async () => {
+        const res = await fetch(`https://fakestoreapi.com/products/${id}`);
+        const datos = await res.json();
+        setProducto(datos);
+    };
+    if (id) {
+      obtenerDetalleProducto();
+    }
+  }, [id]);
+  if (!producto) {
+    return <p>Cargando...</p>;
+  }
+
+
   return (
     <>
       <Nav />
-
       <section className={style.container}>
-        <Image
-          src={producto.image}
-          alt={`Foto de ${producto.title}`}
-          width={200}
-          height={200}
-          className={style.imageproduct}
-        />
+        <Image src={producto.image} alt={`Foto de ${producto.title}`} width={200} height={200} className={style.imageproduct} />
         <section className={style.textcontain}>
-
           <h1 className={style.titulo}>{producto.title}</h1>
-          <h2 className={style.categoria}>{producto.category}</h2>
+          <h2 className={style.categoria}>{producto.category}</h2><span> ${ producto.price }</span>
           <p>{producto.description}</p>
 
-          <Link href={`./productos`} className={style.btn}>
-            Comprar
-          </Link>
+          <button onClick={() => {
+            setCart({
+              products: [...cart.products, producto],
+              totalPrice: cart.totalPrice + producto.price, 
+            })
+          }}
+          className={style.btn}>
+            Agregar al carrito
+          </button>
+        
         </section>
       </section>
-    </>
+      </>
   );
 };
-
-export async function getServerSideProps({
-  query: { id },
-}: {
-  query: { id: string };
-}) {
-  const url = `https://fakestoreapi.com/products/${id}`;
-  const respuesta = await fetch(url);
-  const producto = await respuesta.json();
-  return {
-    props: {
-      producto,
-    },
-  };
-}
 
 export default DetalleDelProducto;
