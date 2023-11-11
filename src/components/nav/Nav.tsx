27833ff862c7fb38a6bfb3c75e608d7d93a6ddc1
NehemiasBalsas <brandonbalsas7@gmail.com>
2023-11-11 15:32:12 -0300

    chat sin hacer

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,39 +1,104 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import style from '@/styles/navar.module.css';
 import search from '@/assets/img/search.png';
 import setting from '@/assets/img/setting.png';
 import carrito from '@/assets/img/shopping-cart (3).png';
 import Cart from '../Cart';
+import Link from 'next/link';
+import css from '@/styles/productos.module.css';
+
+interface Product {
+  id: number;
+  title: string;
+  price: number;
+  image: string;
+}
 
 export default function Nav() {
   const [cartOpen, setCartOpen] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchVisible, setSearchVisible] = useState(false);
+  const [searchResults, setSearchResults] = useState<Product[]>([]);
+  const apiUrl = 'https://fakestoreapi.com/products';
 
   const handleCartToggle = () => {
     setCartOpen(!cartOpen);
   };
 
+  const handleSearchToggle = () => {
+    setSearchVisible(!searchVisible);
+  };
+
+  const handleSearch = async () => {
+    try {
+      const response = await fetch(`${apiUrl}?title=${searchTerm}`);
+      const data = await response.json();
+      setSearchResults(data as Product[]);
+    } catch (error) {
+      console.error('Error al buscar productos:', error);
+    }
+  };
+
+ 
+
   return (
     <div>
       <nav className={style.navbarcontainer}>
-        {/* Botón buscar */}
-        <button>
+        {searchVisible && (
+          <div className={css.searchContainer}>
+            <input
+              type="text"
+              placeholder="Buscar productos..."
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              className={css.searchInput}
+            />
+            <button className={css.searchButton} onClick={handleSearch}>Buscar</button>
+          </div>
+        )}
+
+        <button onClick={handleSearchToggle}>
           <Image src={search} alt="foto no disponible" className={style.buscador} />
         </button>
 
-        {/* Botón filtros */}
         <button>
           <Image src={setting} alt="foto no disponible" className={style.menu} />
         </button>
 
-        {/* Botón del carrito */}
         <button onClick={handleCartToggle}>
           <Image src={carrito} alt="Cart Icon" className={style.carrito} />
         </button>
       </nav>
 
-      {/* Renderizar el componente Cart si cartOpen es true */}
       {cartOpen && <Cart />}
+
+      {searchResults.length > 0 && (
+        <>
+          <div className={css.gridcontainer}>
+        {searchResults.map((producto) => (
+             <article className={css.cardcontain} key={producto.id}>
+            <Image
+              src={producto.image}
+              alt={`Foto de ${producto.title}`}
+              width={200}
+              height={200}
+              layout='resposive'
+              className={css.imagedit}
+            />
+
+            <div className={css.textcontain}>
+              <h2 className={css.titulo}>{producto.title}</h2>
+              <p className={css.price}>$ {producto.price}</p>
+              <Link className={css.btn} href={`./productos/lista/${producto.id}`}>
+                Ver más
+              </Link>
+            </div>
+          </article>
+        ))}
+        </div>
+        </>
+      )}
     </div>
   );
 }
