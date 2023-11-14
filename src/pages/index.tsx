5ccb3c9f40c845d5c17c76171bf6f688b7614768
NehemiasBalsas <brandonbalsas7@gmail.com>
2023-11-13 23:28:10 -0300

    chat hecho pa

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import Inicio from "@/components/Inicio"
 import { Inter } from 'next/font/google'
 import Nav from '@/components/nav/Nav'
+import Chat from '@/components/Chat'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,6 +11,7 @@ export default function Home() {
     <main>
       <Nav />
       <Inicio />
+      <Chat />
     </main>
   )
 }
